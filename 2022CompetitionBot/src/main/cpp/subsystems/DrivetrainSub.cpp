227f2eff8afde0bb19a0da8513ac0e76a7e86e7e
Aidan Fish3r <d0nt73day@gmail.com>
2022-01-26 20:32:55 -0500

    Added autoshift and encoder functions.
    Tip: use shift+alt to vertical select

--- 2022CompetitionBot/src/main/cpp/subsystems/DrivetrainSub.cpp
+++ 2022CompetitionBot/src/main/cpp/subsystems/DrivetrainSub.cpp
@@ -4,12 +4,19 @@
 
 #include "subsystems/DrivetrainSub.h"
 
+constexpr double kShiftUpSpeed = 2.35; //meters per second
+constexpr double kShiftDownSpeed = 1.15; 
+
+constexpr double kEncoderRotationsToMetersLowGear = 1.; //Find these actual values
+constexpr double kEncoderRotationsToMetersHighGear = 1.;
+
 DrivetrainSub::DrivetrainSub() = default;
 
 void DrivetrainSub::init() { //Reset all hardware to a safe state
     tankDrive(0.,0.);
     shiftDown();
-    
+    zeroDrivetrainEncoders();
+
     m_leftMotor1.SetInverted(false); //Find which motor is acutally reversed and you can remove the false SetInverted functions
     m_leftMotor2.SetInverted(true);
     m_leftMotor3.SetInverted(false);
@@ -32,6 +39,9 @@ void  DrivetrainSub::tankDrive(double lPower, double rPower) {
     m_rightMotor2.Set(rPower);
     m_rightMotor3.Set(-rPower);
 }
+void DrivetrainSub::arcadeDrive(double drivePwr, double rotatePwr) {
+    m_drive.ArcadeDrive(drivePwr,rotatePwr);
+}
 void  DrivetrainSub::shiftUp() { //Gear shift up
     m_shifter.Set(1);
 }
@@ -39,6 +49,52 @@ void  DrivetrainSub::shiftDown() { // Gear shift down
     m_shifter.Set(0);
 }
 
-void DrivetrainSub::arcadeDrive(double drivePwr, double rotatePwr) {
-    m_drive.ArcadeDrive(drivePwr,rotatePwr);
+bool DrivetrainSub::isShiftedInHighGear() {
+    return m_shifter.Get();
+}
+void DrivetrainSub::autoShift() {
+    //get speed from robot
+    double averageWheelSpeed = (getLeftVelocity() + getRightVelocity()) / 2.;
+
+    if(fabs(averageWheelSpeed) > kShiftUpSpeed) {
+        shiftUp();
+    }
+    else if(fabs(averageWheelSpeed) < kShiftDownSpeed) {
+        shiftDown();
+    }
+}
+
+void DrivetrainSub::zeroDrivetrainEncoders() {
+    m_leftMotor1.GetEncoder().SetPosition(0.);
+    m_leftMotor2.GetEncoder().SetPosition(0.);
+    m_leftMotor3.GetEncoder().SetPosition(0.);
+
+    m_rightMotor1.GetEncoder().SetPosition(0.);
+    m_rightMotor2.GetEncoder().SetPosition(0.);
+    m_rightMotor3.GetEncoder().SetPosition(0.);
+}
+
+double DrivetrainSub::getLeftEncoderRaw() { //Returns rotations (1 full motor rotation = 1.)
+    return -m_leftMotor1.GetEncoder().GetPosition(); //verify which encoder is reversed
+}
+double DrivetrainSub::getRightEncoderRaw() { 
+    return m_rightMotor1.GetEncoder().GetPosition();
+}
+
+double DrivetrainSub::getLeftEncoderDistanceM() {
+    return getLeftEncoderRaw() * getEncoderRotationsToMeterFactor();
+}
+double DrivetrainSub::getRightEncoderDistanceM() {
+    return getLeftEncoderRaw() * getEncoderRotationsToMeterFactor();
+}
+
+double DrivetrainSub::getEncoderRotationsToMeterFactor() {
+    return (isShiftedInHighGear()) ? kEncoderRotationsToMetersHighGear : kEncoderRotationsToMetersLowGear;
+}
+
+double DrivetrainSub::getLeftVelocity() {
+    return m_leftMotor1.GetEncoder().GetVelocity() * getEncoderRotationsToMeterFactor() * 60.; //In meters per second
+}
+double DrivetrainSub::getRightVelocity() {
+    return m_rightMotor1.GetEncoder().GetVelocity() * getEncoderRotationsToMeterFactor() * 60.; //In meters per second
 }
