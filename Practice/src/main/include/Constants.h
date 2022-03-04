d4e648c43637a245f1aa54d06287358699fa1418
4917 Sonny <software@4917.ca>
2022-03-04 14:23:52 -0500

    Practice Code Stuff

--- Practice/src/main/include/Constants.h
+++ Practice/src/main/include/Constants.h
@@ -4,6 +4,12 @@
 
 #pragma once
 
+namespace CanIds {
+    // Sort these in CAN ID order
+    constexpr int kLeftMotor1 = 1;
+    constexpr int kRightMotor1 = 4;
+}
+
 /**
  * The Constants header provides a convenient place for teams to hold robot-wide
  * numerical or boolean constants.  This should not be used for any other
--- Practice/src/main/include/subsystems/ExampleSubsystem.h
+++ Practice/src/main/include/subsystems/ExampleSubsystem.h
@@ -4,11 +4,16 @@
 
 #pragma once
 
+#include <rev/CANSparkMax.h>
 #include <frc2/command/SubsystemBase.h>
+#include <constants.h>
 
 class ExampleSubsystem : public frc2::SubsystemBase {
  public:
   ExampleSubsystem();
+  void tankDrive(double leftPower, double rightPower);
+    
+
 
   /**
    * Will be called periodically whenever the CommandScheduler runs.
@@ -22,6 +27,11 @@ class ExampleSubsystem : public frc2::SubsystemBase {
   void SimulationPeriodic() override;
 
  private:
+rev::CANSparkMax m_leftMotor1{CanIds::kLeftMotor1, 
+                          rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+  rev::CANSparkMax m_rightMotor1{CanIds::kRightMotor1, 
+                          rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 };
