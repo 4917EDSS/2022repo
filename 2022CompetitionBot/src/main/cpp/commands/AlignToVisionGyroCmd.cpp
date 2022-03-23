7929d20f0746ad96119d0956724008170ef448a9
4917 Tardis <software@4917.ca>
2022-03-22 20:02:02 -0400

    Updated AlignToGyro to only look for target if it isn't at a 0 degree angle
    doesn't turn properly on floor, maybe works on carpet

--- 2022CompetitionBot/src/main/cpp/commands/AlignToVisionGyroCmd.cpp
+++ 2022CompetitionBot/src/main/cpp/commands/AlignToVisionGyroCmd.cpp
@@ -4,6 +4,7 @@
 
 #include "commands/AlignToVisionGyroCmd.h"
 #include <frc/RobotController.h>
+#include <iostream>
 
 constexpr double kMinPower = 0.4;
 
@@ -17,27 +18,41 @@ AlignToVisionGyroCmd::AlignToVisionGyroCmd(DrivetrainSub *drivetrainSub, VisionS
 void AlignToVisionGyroCmd::Initialize() {
   m_visionSubPtr->targetVisionPipeline();
   m_startTime = frc::RobotController::GetFPGATime();
-  m_angle = m_visionSubPtr->getHorizontalAngle();
+  
+  m_angle = m_visionSubPtr->getHorizontalAngle(); 
+  m_drivetrainSubPtr->zeroHeading(); 
 }
+//-12 deg   -- 0 deg
+// -12-0 = -12
+//  -12/20.5 =/ -0.5
+//  dir = -1
+//  p=0.5
+//  a = 12
 
 // Called repeatedly when this Command is scheduled to run
 void AlignToVisionGyroCmd::Execute() { //Basically copied from RotateRobotCmd
-  double angleRemaining = m_angle-m_drivetrainSubPtr->getHeading();
-  double rotationPower = (angleRemaining/20);
-  double dir = (angleRemaining < 0) ? -1: 1;
-  rotationPower = fabs(rotationPower);
-  angleRemaining = fabs(angleRemaining);
-  if(rotationPower < kMinPower && rotationPower > 0){ rotationPower = kMinPower; }
+  if( m_visionSubPtr->getHorizontalAngle() == 0.0) m_angle =  m_visionSubPtr->getHorizontalAngle();
+  else {
+    m_angle = m_visionSubPtr->getHorizontalAngle(); 
+    double angleRemaining = m_angle-m_drivetrainSubPtr->getHeading();
+    double rotationPower = (angleRemaining/20.5);
+    double dir = (angleRemaining < 0.0) ? -1.0: 1.0;
+    rotationPower = fabs(rotationPower);
+    angleRemaining = fabs(angleRemaining);
+    if(rotationPower < kMinPower && rotationPower > 0){ rotationPower = kMinPower; }
 
-  if(angleRemaining < .5){
-    m_drivetrainSubPtr->arcadeDrive(0, 0);
-  } else {
-    m_drivetrainSubPtr->arcadeDrive(0, rotationPower*dir);
-  }
+    if(angleRemaining < .5){ //.5 degrees
+      m_drivetrainSubPtr->arcadeDrive(0, 0);
+    } else {
+      m_drivetrainSubPtr->arcadeDrive(0, rotationPower*dir);
+    }
+  } 
 }
 
 // Called once the command ends or is interrupted.
-void AlignToVisionGyroCmd::End(bool interrupted) {}
+void AlignToVisionGyroCmd::End(bool interrupted) {
+  m_visionSubPtr->targetNeutralVisionPipeline(); //Turn off camera
+}
 
 // Returns true when the command should end.
 bool AlignToVisionGyroCmd::IsFinished() {
