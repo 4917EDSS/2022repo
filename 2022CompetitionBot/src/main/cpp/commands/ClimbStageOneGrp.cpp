0a64bee450f1e3c5a739eb6f7ac629f264c7dd78
JasonFitch4917 <jasonfitch@4917.ca>
2022-04-04 10:32:46 -0400

    Adjusted climb auto TODO comments.

--- 2022CompetitionBot/src/main/cpp/commands/ClimbStageOneGrp.cpp
+++ 2022CompetitionBot/src/main/cpp/commands/ClimbStageOneGrp.cpp
@@ -13,36 +13,58 @@ ClimbStageOneGrp::ClimbStageOneGrp(ClimberSub *climberSub) {
   //   2. mast arm raised (extended) all the way
   //   3. mast arm shaft against medium rung (so mast will hook from rear of medium rung)
 
+  // NOTES: 1. To fully lower mast arm pulling robot up from lowest to highest position
+  //           takes roughly 1.4 seconds.
+  //        2. To fully unfold (extend) hook arms from completely folded in (retracted)
+  //           position takes roughly 1 second.
+  //        3. To fully fold in (retract) hook arms from completely unfolded (extended)
+  //           position while hanging and swining on hook arms takes roughly 1 second when
+  //           unhooking from medium rung to hang and swing from high rung - the hook arms
+  //           retract in 2 phases pausing momentarily between phases as they fight the swing.
+
   // TODO: We need more granularity with delays (more tweaking options) and should move all
   //       delays out of the raise/lower and latch/release commands to make this easier to
   //       tweak and understand. Do this in steps as follows:
+  //
   //       1. Create a DelayCmd command that takes a double parameter named delaySeconds and
-  //          simply completes once that amount of time has passed.
+  //          simply completes once that amount of time has passed. The new commmand needs
+  //          a m_startTime member variable and logic in IsFinished similar to
+  //          ClimberArmsLatchReleaseCmd (but calculate the delay time in micro seconds
+  //          from the supplied delaySeconds).
+  //
   //       2. Add calls to new DelayCmd wherever any of these command calls delay:
   //              ClimberArmsLatchReleaseCmd
   //              ClimberArmLowerCmd
   //              ClimberArmRaiseCmd
   //          Do this with the intent that *ALL* delays in those commands will be removed.
+  //
   //       3. Remove all delays in the above 3 existing commands - all start and end delays
   //          and all related parameters and member variables.
-  //       4. Change ClimberArmRaiseCmd bool isPartial parameter to be a double parameter named
-  //          raisePercentage and make the command determine the target climb height based on
-  //          the requested percentage of kClimberArmMaxHeight. Also rename climberArmMaxHeight
-  //          in ClimberArmRaiseCmd::IsFinished to ba nameed targetClimbHeight to better
-  //          represent what it is.
+  //
+  //       4. Add kClimberArmMinHeight to ClimberArmRaiseCmd (copy from ClimberArmLowerCmd).
+  //          Change ClimberArmRaiseCmd bool isPartial parameter to be a double parameter named
+  //          targetHeightPercentage and make the command determine the target climb height
+  //          based on the requested percentage. Also rename climberArmMaxHeight in
+  //          ClimberArmRaiseCmd::IsFinished to be nameed
+  //          targetClimbHeight to better represent what it is.
   //          NOTES:
-  //            * We currently divide by 3 which would be equivalent to passing in 33.333 for
-  //              raisePercentage after these changes are made.
-  //            * To fully raise, we would pass in 100.0 for raisePercentage.
-  //       5. Change ClimberArmLowerCmd to add a double parameter named lowerPercentage and
-  //          make the command determine the target height to lower to based on the requested
-  //          percentage of kClimberArmMinHeight. Make this similar to ClimberArmRaiseCmd.
+  //            * We currently divide by 3 which would be similar to passing in 33.333 for
+  //              targetHeightPercentage after these changes are made.
+  //            * To fully raise, we would pass in 100.0 for targetHeightPercentage.
+  //
+  //       5. Add kClimberArmMaxHeight to ClimberArmLowerCmd (copy from ClimberArmRaiseCmd).
+  //          Change ClimberArmLowerCmd to add a double parameter named targetHeightPercentage
+  //          and make the command determine the target height to lower the mast to based on
+  //          the requested percentage. Make this similar to ClimberArmRaiseCmd.
   //          NOTES:
   //            * We may need to partially lower the mast arm and this will let us.
-  //            * To fully lower, we would pass in 100.0 for lowerPercentage.
+  //            * To fully lower, we would pass in 0.0 for targetHeightPercentage.
+  //
   //       6. At this point, the code execution behaviour should be identical to what it was
   //          before these changes.
+  //
   //       7. Make changes as indicated by TODO comments below.
+  //
   //       8. Test and tweak delays and raise/lower percentages.
 
   // Climb from medium to traversal rungs.
@@ -52,35 +74,38 @@ ClimbStageOneGrp::ClimbStageOneGrp(ClimberSub *climberSub) {
     // STAGE 1
     // ---------------------------------------------------------------------------------------
     
-    // 1. Unfold (extend) hook arms - no delays:
+    // TODO: 1. Partially lower (retract) mast arm by 3-5% only to secure the mast hook on the
+    //          medium rung before unfolding (extending) hook arms (this helps avoid being
+    //          bumped out of position while the hook arms extend):
+    // 2. Unfold (extend) hook arms - no delays:
     ClimberArmsLatchReleaseCmd(climberSub, false, false, false),
-    // 2. Fully lower (retract) mast arm, lifting robot as high as possible:
+    // 3. Fully lower (retract) mast arm, lifting robot as high as possible:
     ClimberArmLowerCmd(climberSub),
 
     // Robot is now hanging by mast arm from medium rung.
     // Hook arms are ready to grab high rung from rear.
     // Robot is not swinging.
 
-    // 3. Fold in (retract) hook arms - no delays:
+    // 4. Fold in (retract) hook arms - no delays:
     ClimberArmsLatchReleaseCmd(climberSub, true, false, false),
-    // 4. Partially raise (extend) mast arm to lower robot and unhook mast from medium rung:
+    // 5. Partially raise (extend) mast arm to lower robot and unhook mast from medium rung:
     ClimberArmRaiseCmd(climberSub, true),
 
     // Robot is now detached from medium rung.
     // Robot is swinging on high rung by hook arms.
     // Mast is not fully extended yet.
 
-    // 5. Fold in (retract) hook arms - delay at end:
+    // 6. Fold in (retract) hook arms - delay at end:
     ClimberArmsLatchReleaseCmd(climberSub, true, false, true),
-    // 6. Fully raise (extend) mast arm => mast arm hook *MUST* end up on rear side of high rung:
+    // 7. Fully raise (extend) mast arm => mast arm hook *MUST* end up on rear side of high rung:
     ClimberArmRaiseCmd(climberSub, false),
 
     // Attempt to recover from mast hook ending up on the wrong side (front) of high rung.
     // If we have this condition, the back of the mast hook will be rubbing against the front
     // of the high rung and that with a little time will help the swing slow down.
-    // TODO: 7. Partially lower (retract) mast arm (e.g. maybe by 15%) making mast hook slightly
-    //       lower than high rung.
-    // TODO: 8. Fully raise (extend) mast arm and hope mast hook ends up on rear side of high rung.
+    // TODO: 8. Partially lower (retract) mast arm (e.g. maybe by 15%) making mast hook slightly
+    //          lower than high rung:
+    // TODO: 9. Fully raise (extend) mast arm and hope mast hook ends up on rear side of high rung:
     // NOTE: This may not really fix anything, but worth a try. Some alternatives to this:
     //           * If we break this group out into stages, the operator could manually lower
     //             and raise the mast arm as desired/required to get mast hook on the rear
@@ -99,45 +124,46 @@ ClimbStageOneGrp::ClimbStageOneGrp(ClimberSub *climberSub) {
     // STAGE 2
     // ---------------------------------------------------------------------------------------
 
-    // 9. Delay at start - unfold (extend) hook arms to press mast against rear side of high rung:
+    // 10. Delay at start - unfold (extend) hook arms to press mast against rear side of high rung:
     ClimberArmsLatchReleaseCmd(climberSub, false, true, false),
-    // 10. Fully lower (retract) mast arm to lift robot and allow hook arms to pop off high rung:
+    // 11. Fully lower (retract) mast arm to lift robot and allow hook arms to pop off high rung:
     ClimberArmLowerCmd(climberSub),
 
     // Robot is swinging on high rung by mast arm.
     // Hook arms are fully extended or on the way to be.
+
     // We found that the aggressive unhook action can make the robot twist and hook arms not
     // approach the traversal rung evenly - one hook can properly end up on rear side, but
     // other hook can end up on the front side and our climb must be abruptly stopped when
     // that happens to avoid bend damage.
     // To avoid this we can try this:
-    //    (a) Make step 10 above partially lower by 3-5% only - just enough to get the mast
+    //    (a) Make step 11 above partially lower by 3-5% only - just enough to get the mast
     //        hook to lift the hook arms slightly off the high rung.
     //    (b) Add a command to reverse the hook arms to fold them in (retract) so they don't
     //        aggressively unhook.
     //    (c) Add a command to partially lower to around 15% so that hook arms' hooks are
     //        clear of high rung.
     //    (d) Add a command to fully unfold (extend) hook amrs.
-    //    (e) Add a command to delay as necessary before performing step 11.
+    //    (e) Add a command to delay as necessary before performing step 12 below.
     // That removes the aggressive unhook and should avoid inducing wobble.
-    // We may need to duplicate steps 7 and 8 next before performing step 11 or break the
-    // rest of the commands below into stage 3 and rely on operator to manually fix hook
+    // We may need to duplicate steps 8 and 9 next before performing step 12 below or break
+    // the rest of the commands below into stage 3 and rely on operator to manually fix hook
     // arm placement.
 
     // ---------------------------------------------------------------------------------------
     // STAGE 3
     // ---------------------------------------------------------------------------------------
 
-    // 11. Fold in (retract) hook arms - no delays:
+    // 12. Fold in (retract) hook arms - no delays:
     ClimberArmsLatchReleaseCmd(climberSub, true, false, false)
 
-    // TODO: 12. Partially raise (extend) mast arm to lower robot and unhook mast from high rung.
+    // TODO: 13. Partially raise (extend) mast arm to lower robot and unhook mast from high rung.
     
     // Robot is swinging on traversal rung by mast arm.
     // Hook arms are becoming fully folded in (retracted).
     // Robot should end up straight.
 
-    // TODO: 13. Fully lower (retract) mast arm, returning it to start position for next match.
+    // TODO: 14. Fully lower (retract) mast arm, returning it to start position for next match.
   );
 }
 
